bc6c2328842469a7d94105e103145a83eca87d87
aiaaua <55227276+aiaaua@users.noreply.github.com>
2020-01-24 01:59:32 +0900

    Update #5545번_최고의 피자.py

--- Greedy/#5545번_최고의 피자.py
+++ Greedy/#5545번_최고의 피자.py
@@ -5,7 +5,7 @@ t_kcal = sorted([int(input()) for _ in range(t_num)], reverse = True)
 
 kcal = d_kcal/d_price
 
-for i in range(1, len(t_kcal)) :
+for i in range(len(t_kcal)) :
     if kcal < (d_kcal + sum(t_kcal[:i]))/(d_price + t_price*i) :
         kcal = (d_kcal + sum(t_kcal[:i]))/(d_price + t_price*i)
 
