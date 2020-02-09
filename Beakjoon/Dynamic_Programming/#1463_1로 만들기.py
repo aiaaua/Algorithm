cc7f81842966b93cdf7ba86f8f951796a1707ed5
aiaaua <55227276+aiaaua@users.noreply.github.com>
2020-02-10 00:32:29 +0900

    Update #1463_1로 만들기.py

--- Beakjoon/Dynamic_Programming/#1463_1로 만들기.py
+++ Beakjoon/Dynamic_Programming/#1463_1로 만들기.py
@@ -1,10 +1,10 @@
 n = int(input())
 
-result = [0 for _ in range(n+1)]
+result = [0 for _ in range(n + 1)]
 
-for i in range(2, N + 1):
+for i in range(2, n + 1):
     result[i] = result[i-1] + 1
     if i % 3 == 0 : result[i] = min(result[i//3] + 1, result[i])
     if i % 2 == 0 : result[i] = min(result[i//2] + 1, result[i])
         
-print(result[N])
+print(result[n])
