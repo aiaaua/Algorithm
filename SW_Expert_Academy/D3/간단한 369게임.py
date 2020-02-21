56ead3930e75a52a04d45d960aab74f1895ea379
aiaaua <55227276+aiaaua@users.noreply.github.com>
2020-02-21 23:55:45 +0900

    Update 간단한 369게임.py

--- SW_Expert_Academy/D3/간단한 369게임.py
+++ SW_Expert_Academy/D3/간단한 369게임.py
@@ -4,7 +4,5 @@ for i in range(1, n+1) :
     L = list(str(i))
     num = 0
     num = L.count("3") + L.count("6") + L.count("9")
-    if num == 0 :
-        print(i, '', end = '')
-    else : 
-        print('-' * num, '', end = '')
+    if num == 0 : print(i, '', end = '')
+    else : print('-' * num, '', end = '')
