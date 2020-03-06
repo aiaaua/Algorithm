b4b82f83274d995a60013b163919daa971f3c420
aiaaua <55227276+aiaaua@users.noreply.github.com>
2020-03-07 00:08:55 +0900

    Update #1110_더하기사이클.py

--- Beakjoon/Mathematics/#1110_더하기사이클.py
+++ Beakjoon/Mathematics/#1110_더하기사이클.py
@@ -1,10 +1,9 @@
-import copy
-
 target = int(input())
-a = copy.deepcopy(target)
-cnt = 0
+a = target
+cnt = int()
 
 while True :
     cnt += 1
     a = (a%10)*10 + (a//10 + a%10)%10
-    if a == target : print(cnt);break
+    if a == target : break
+print(cnt)
