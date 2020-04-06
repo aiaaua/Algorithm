829d79d6df3081a322b377215069fdcea3e2630c
aiaaua <55227276+aiaaua@users.noreply.github.com>
2020-04-07 00:17:46 +0900

    Update #1193_분수찾기.py

--- Beakjoon/Mathematics/#1193_분수찾기.py
+++ Beakjoon/Mathematics/#1193_분수찾기.py
@@ -1,7 +1,11 @@
 target = int(input())
-i, s = 2, 1
-while target > s :
-    s += i
-    i += 1
-a = [s-target+1, i-s+target-1][i%2]
-print(a, "/", i-a, sep = '')
+s_num = 2
+count = 1
+
+while target > count :
+    count += s_num; s_num += 1
+
+if s_num%2 == 0 : x = 1 + count - target
+else : x = s_num - 1 - (count - target)
+    
+print(x, "/", s_num-x, sep = "")
