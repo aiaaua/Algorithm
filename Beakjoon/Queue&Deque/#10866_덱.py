8b18a0c7e3229d43e000713ce5f65bdd60b44297
aiaaua <55227276+aiaaua@users.noreply.github.com>
2020-04-14 01:25:31 +0900

    Add files via upload

---
+++ Beakjoon/Queue&Deque/#10866_덱.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+
+num = int(input())
+commend = [list(sys.stdin.readline().split()) for _ in range(num)]
+q = deque()
+
+for i in range (num) :
+    if commend[i][0] == "push_front" : q.appendleft(commend[i][1])
+    elif commend[i][0] == "push_back" : q.append(commend[i][1])
+    elif commend[i][0] == "pop_front" :
+        if len(q) >= 1 : print(q.popleft())
+        else : print(-1)
+    elif commend[i][0] == "pop_back" :
+        if len(q) >= 1 : print(q.pop())
+        else : print(-1)
+    elif commend[i][0] == "size" : print(len(q))
+    elif commend[i][0] == "empty" :
+        if len(q) == 0 : print(1)
+        else : print(0)
+    elif commend[i][0] == "front" :
+        if len(q) >= 1 : print(q[0])
+        else : print(-1)
+    elif commend[i][0] == 'back' :
+        if len(q) >= 1 : print(q[-1])
+        else : print(-1)
