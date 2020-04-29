67a2da9158c9ba04e01446f1ce48aef69d6ad4ec
aiaaua <55227276+aiaaua@users.noreply.github.com>
2020-04-29 23:55:37 +0900

    Update #Level1_모의고사.py

--- Programmers/#Level1_모의고사.py
+++ Programmers/#Level1_모의고사.py
@@ -5,19 +5,15 @@ def solution(answers):
     cnt = [0, 0, 0]
     
     for i in range(len(answers)) : 
-        if answers[i] == a[i%5] : 
-            cnt[0] += 1
+        if answers[i] == a[i%5] : cnt[0] += 1
     for i in range(len(answers)) : 
-        if answers[i] == b[i%8] : 
-            cnt[1] += 1
+        if answers[i] == b[i%8] : cnt[1] += 1
     for i in range(len(answers)) : 
-        if answers[i] == c[i%10] : 
-            cnt[2] += 1
+        if answers[i] == c[i%10] : cnt[2] += 1
 
     answer = []
     x = max(cnt)
     for i in range(3) :
-        if cnt[i] == x :
-            answer.append(i+1)
+        if cnt[i] == x : answer.append(i+1)
     
     return answer
