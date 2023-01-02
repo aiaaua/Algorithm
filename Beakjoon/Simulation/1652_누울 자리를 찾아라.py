8437d0d96069bd0d6deb3f38f58280815d62b477
aiaaua <55227276+aiaaua@users.noreply.github.com>
2023-01-03 00:21:40 +0900

    Update 1652_누울 자리를 찾아라.py

--- Beakjoon/Simulation/1652_누울 자리를 찾아라.py
+++ Beakjoon/Simulation/1652_누울 자리를 찾아라.py
@@ -1,25 +1,29 @@
-n = int(input())
-li = []
-r, c = 0, 0
-cnt = 0
-for i in range(n):
-    li.append(list(input()))
-for i in range(n):
-    cnt = 0
-    for j in range(n):
-        if li[i][j] == '.':
-            cnt += 1
+N = int(input())
+room = [list(input()) for _ in range(N)]
+
+length, width = 0, 0
+
+for i in range(N):
+    length_cnt, width_cnt = 0, 0
+    for j in range(N):
+        if room[i][j] == '.':
+            length_cnt += 1
         else:
-            cnt = 0
-        if cnt == 2:
-            r += 1
-for i in range(n):
-    cnt = 0
-    for j in range(n):
-        if li[j][i] == '.':
-            cnt += 1
+            if length_cnt >= 2:
+                length += 1
+            length_cnt = 0
+            
+        if room[j][i] == '.':
+            width_cnt += 1
         else:
-            cnt = 0
-        if cnt == 2:
-            c += 1
-print(r, c)
+            if width_cnt >= 2:
+                width += 1
+            width_cnt = 0
+        
+        if j == N-1:
+            if length_cnt >= 2:
+                length += 1
+            if width_cnt >= 2:
+                width += 1
+
+print(length, width)
